ef7c187d0a72a4201b9a4148e8ebaeeae1f3fe52
Tu <tuvo1106@gmail.com>
2019-09-01 19:51:40 -0700

    Refactor app.js logic for cleaner routing

--- app.js
+++ app.js
@@ -1,102 +1,107 @@
-const fs = require("fs");
-const express = require("express");
+const fs = require('fs')
+const express = require('express')
 
-const app = express();
+const app = express()
 // middleware
-app.use(express.json());
+app.use(express.json())
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+)
 
-// get
-app.get("/api/v1/tours", (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
-    status: "success",
+    status: 'success',
     results: tours.length,
     data: {
       tours
     }
-  });
-});
+  })
+};
 
-// variable url
-// ? makes it optional
-app.get("/api/v1/tours/:id", (req, res) => {
+const getTourByID = (req, res) => {
   // console.log(req.params); { id: 5 }
   // convert string to number
-  const id = req.params.id * 1;
+  const id = req.params.id * 1
 
   // find element in tours array
-  const tour = tours.find(el => el.id === id);
+  const tour = tours.find(el => el.id === id)
   if (!tour) {
     return res.status(404).json({
-      status: "fail",
-      message: "invalid ID"
-    });
+      status: 'fail',
+      message: 'invalid ID'
+    })
   }
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
       tour: tour
     }
-  });
-});
+  })
+};
 
-// get
-app.post("/api/v1/tours", (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+  tours.push(newTour)
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     err => {
-      if (err) console.log("file not found");
+      if (err) console.log('file not found')
       res.status(201).json({
-        status: "success",
+        status: 'success',
         data: {
           tour: newTour
         }
-      });
+      })
     }
-  );
-});
+  )
+};
 
-// patch - expect properties that will be updated, not whole JSON
-app.patch("/api/v1/tours/:id", (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
-      status: "fail",
-      message: "invalid ID"
-    });
+      status: 'fail',
+      message: 'invalid ID'
+    })
   }
   // will not implement patch with local storage
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
-      tour: "<updated tour"
+      tour: '<updated tour'
     }
-  });
-});
+  })
+};
 
-// delete
-app.delete("/api/v1/tours/:id", (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
-      status: "fail",
-      message: "invalid ID"
-    });
+      status: 'fail',
+      message: 'invalid ID'
+    })
   }
   // will not implement patch with local storage
   // 204 means no content
   res.status(204).json({
-    status: "success",
+    status: 'success',
     data: null
-  });
-});
+  })
+};
 
-const port = 3000;
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour)
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTourByID)
+  .patch(updateTour)
+  .delete(deleteTour)
+
+const port = 3000
 app.listen(port, () => {
-  console.log(`app running on port ${port}...`);
-});
+  console.log(`app running on port ${port}...`)
+})
