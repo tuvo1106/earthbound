1f7d08e32277d739c49f21607d51ea4a2915945f
Tu <tuvo1106@gmail.com>
2019-09-01 21:14:52 -0700

    Add nodemon to package.json, refactor server.js

--- app.js
+++ app.js
@@ -19,8 +19,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-// start server
-const port = 3000
-app.listen(port, () => {
-  console.log(`app running on port ${port}...`)
-})
+module.exports = app
