e187ded37bf1a2c20943f8fc2f18b00833408e8f
Tu <tuvo1106@gmail.com>
2019-09-03 11:51:52 -0700

    Add filtering to api queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,11 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // copy object
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+    const tours = await Tour.find(queryObj);
     res.status(200).json({
       status: 'success',
       results: tours.length,
