ef649297deecf691624aa332b0dc2a0a971e1baf
Tu <tuvo1106@gmail.com>
2019-09-02 23:39:04 -0700

    Refactor get methods to use mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,28 +1,39 @@
 /* eslint-disable semi */
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success'
-    // results: tours.length,
-    // data: {
-    //   tours
-    // }
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.getTourByID = (req, res) => {
-  // console.log(req.params); { id: 5 }
-  // convert string to number
-  const id = req.params.id * 1;
-  // find element in tours array
-  // const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success'
-    // data: {
-    //   tour: tour
-    // }
-  });
+exports.getTourByID = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.send(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
