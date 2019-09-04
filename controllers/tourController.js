8c27eb770b745940e3c1b0ffb15a2c10652f4251
Tu <tuvo1106@gmail.com>
2019-09-03 17:43:26 -0700

    Debug delete route to send back correct logging

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -80,7 +80,7 @@ exports.updateTour = async (req, res) => {
       }
     });
   } catch (err) {
-    res.send(404).json({
+    res.status(404).json({
       status: 'fail',
       message: err
     });
@@ -88,14 +88,14 @@ exports.updateTour = async (req, res) => {
 };
 
 exports.deleteTour = async (req, res) => {
-  await Tour.findByIdAndDelete(req.params.id);
   try {
+    await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
       data: null
     });
   } catch (err) {
-    res.send(404).json({
+    res.status(404).json({
       status: 'fail',
       message: err
     });
