27cbb757d219c29631e9e67ebb455b88de3e9866
Tu <tuvo1106@gmail.com>
2019-09-03 21:45:36 -0700

    Add error handling for invalid IDs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,48 @@
+const AppError = require('./../utils/appError')
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`
+  return new AppError(message, 400)
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack
+  })
+};
+
+const sendErrorProd = (err, res) => {
+  // operation, trusted error, send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    })
+  } else {
+    // unknown error
+    console.error('ERROR', err)
+    res.status(500).json({
+      status: 'error',
+      message: '🔥 Something went very wrong... 🔥'
+    })
+  }
+}
+
 module.exports = (err, req, res, next) => {
   // internal server error
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-};
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res)
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err }
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error)
+    }
+    sendErrorProd(error, res)
+  }
+}
