327574dae888cdd1c3c29c1c90ccf443e720c27c
Tu <tuvo1106@gmail.com>
2019-09-03 20:43:47 -0700

    Add errorContoller

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
@@ -22,5 +24,15 @@ app.use(express.static(`${__dirname}/public/`))
 // mounting routers
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+// all other routes
+app.all('*', (req, res, next) => {
+  // if anything is passed into next, it will assume there is an error and send
+  // error to global error middleware
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+})
+
+// 4 parameters means global error middleware
+app.use(globalErrorHandler)
 
 module.exports = app
