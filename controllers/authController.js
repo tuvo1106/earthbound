2c300f25ea5b7c3ba92810c9149e319bbc87739c
Tu <tuvo1106@gmail.com>
2019-09-04 23:36:11 -0700

    Add protect middleware for tour routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,14 @@
-const User = require("./../models/userModel");
-const catchAsync = require("./../utils/catchAsync");
-const jwt = require("jsonwebtoken");
-const AppError = require("./../utils/appError");
+const User = require('./../models/userModel')
+const catchAsync = require('./../utils/catchAsync')
+const jwt = require('jsonwebtoken')
+const AppError = require('./../utils/appError')
+// built in promisify
+const { promisify } = require('util')
 
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN
-  });
+  })
 };
 
 exports.signUp = catchAsync(async (req, res, next) => {
@@ -15,34 +17,67 @@ exports.signUp = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
-  });
-  const token = signToken(newUser._id);
+  })
+  const token = signToken(newUser._id)
   res.status(201).json({
-    status: "success",
+    status: 'success',
     token,
     data: {
       user: newUser
     }
-  });
-});
+  })
+})
 
 exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
   // check if email and password exist
   if (!email || !password) {
-    return next(new AppError("Please provide email and password!", 400));
+    return next(new AppError('Please provide email and password!', 400))
   }
   // check if user exists && password is correct
-  const user = await User.findOne({ email }).select("+password");
+  const user = await User.findOne({ email }).select('+password')
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError("Incorrect email or password", 401));
+    return next(new AppError('Incorrect email or password', 401))
   }
 
-  const token = signToken(user._id);
+  const token = signToken(user._id)
   res.status(200).json({
-    status: "success",
+    status: 'success',
     token
-  });
+  })
   // if everything is ok, send token to client
-});
+})
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // get token and check if it exists
+  let token
+  const auth = req.headers.authorization
+  if (auth && auth.startsWith('Bearer')) {
+    token = auth.split(' ')[1]
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    )
+  }
+  // validate token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+
+  // check if user still exists
+  const freshUser = await User.findById(decoded.id)
+  if (!freshUser) {
+    return next(
+      new AppError('The user belonging to this token no longer exists.', 401)
+    )
+  }
+  // check if user changed psw after token was issued
+  if (!freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401)
+    )
+  }
+  // grant access to protected route
+  req.user = freshUser
+  next()
+})
