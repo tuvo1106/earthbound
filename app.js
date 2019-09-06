e082d553d489f111659b6666e30bfc4e37336aff
Tu <tuvo1106@gmail.com>
2019-09-05 23:34:09 -0700

    Add sanitation and more security features

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -34,6 +37,27 @@ app.use('/api', limiter)
 
 // body parser, limit to only 10kb
 app.use(express.json({ limit: '10kb' }))
+
+// sanitize against NoSQL query injections
+app.use(mongoSanitize())
+
+// remove scripts from html
+app.use(xss())
+
+// prevent parameter pollution
+app.use(
+  hpp({
+    // allow for duplicate in query string
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+)
 
 // serve static file
 app.use(express.static(`${__dirname}/public/`))
