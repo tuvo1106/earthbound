1e2dfed40df5dd8f450f67c711eeb08d858445fd
Tu <tuvo1106@gmail.com>
2019-09-05 21:56:15 -0700

    Send cookies to browser with JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,24 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id)
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    // browser cannot modify cookie
+    httpOnly: true
+  }
+
+  // send cookie
+  if (process.env.NODE_ENV === 'production') {
+    // https only
+    cookieOptions.secure = true
+  }
+  res.cookie('jwt', token, cookieOptions)
+
+  // remove password from output
+  user.password = undefined
+
   res.status(statusCode).json({
     status: 'success',
     token,
