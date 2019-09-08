3b885734cbb9bcde05676f99ac1db54a103f0156
Tu <tuvo1106@gmail.com>
2019-09-08 15:14:49 -0700

    Add getTour route

--- controllers/viewsContoller.js
+++ controllers/viewsContoller.js
@@ -4,12 +4,18 @@ const catchAsync = require('./../utils/catchAsync')
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find()
   res.status(200).render('overview', {
+    title: 'Home',
     tours
   })
 })
 
-exports.getTour = (req, res) => {
-  res.status(200).render('overview', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   })
-}
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour
+  })
+})
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const { getOverview, getTour } = require('./../controllers/viewsContoller')
 const router = express.Router()
 
 router.get('/', getOverview)
-router.get('/tour', getTour)
+router.get('/tour/:slug', getTour)
 
 module.exports = router
