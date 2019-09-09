54d01303b79b07fe7153a5f6db3ef5c632640cb6
Tu <tuvo1106@gmail.com>
2019-09-08 17:16:33 -0700

    Add login route and page

--- controllers/viewsContoller.js
+++ controllers/viewsContoller.js
@@ -19,3 +19,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
     tour
   })
 })
+
+exports.login = catchAsync(async (req, res, next) => {
+  res.status(200).render('login', {
+    title: 'Login'
+  })
+})
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express')
-const { getOverview, getTour } = require('./../controllers/viewsContoller')
+const {
+  getOverview,
+  getTour,
+  login
+} = require('./../controllers/viewsContoller')
 
 const router = express.Router()
 
 router.get('/', getOverview)
 router.get('/tour/:slug', getTour)
+router.get('/login', login)
 
 module.exports = router
