39c29b90c289d92a52c8557e814b1402e203f70f
Tu <tuvo1106@gmail.com>
2019-09-15 13:28:04 -0700

    Catch sigterm

--- server.js
+++ server.js
@@ -45,3 +45,10 @@ process.on('unhandledRejection', err => {
     process.exit(1)
   })
 })
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECIEVED. Shutting down gracefully.')
+  server.close(() => {
+    process.exit(1)
+  })
+})
