7186e0c479071c78e981bf016efb2f969d1e5b7d
Tu <tuvo1106@gmail.com>
2019-09-15 08:42:40 -0700

    Create booking after successful purchase

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,7 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const Tour = require('./../models/tourModel')
 const catchAsync = require('./../utils/catchAsync')
-const AppError = require('./../utils/appError')
+const Booking = require('./../models/bookingModel')
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // get the currently booked tour
@@ -9,7 +9,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // create checkout sesion
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: `${req.user.email}`,
     client_reference_id: req.params.tourId,
@@ -29,4 +31,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session
   })
+})
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // only temporary, because it's unsecure, everyone can make booking without paying
+  const { tour, user, price } = req.query
+  if (!tour && !user && !price) return next()
+  await Booking.create({ tour, user, price })
+  // redirect to root url
+  res.redirect(req.originalUrl.split('?')[0])
+  next()
 })
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,10 +8,11 @@ const {
   updateUserData
 } = require('./../controllers/viewsContoller')
 const { isLoggedIn, protect } = require('./../controllers/authController')
+const { createBookingCheckout } = require('./../controllers/bookingController')
 
 const router = express.Router()
 
-router.get('/', isLoggedIn, getOverview)
+router.get('/', createBookingCheckout, isLoggedIn, getOverview)
 router.get('/tour/:slug', isLoggedIn, getTour)
 router.get('/login', isLoggedIn, login)
 router.get('/signup', isLoggedIn, signUp)
