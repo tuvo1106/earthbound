e867ee67f454fd1ea4de6325bed58a2a25eeeca4
Tu <tuvo1106@gmail.com>
2019-09-15 13:59:12 -0700

    Implement CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean')
 const hpp = require('hpp')
 const cookieParser = require('cookie-parser')
 const compression = require('compression')
+const cors = require('cors')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -26,6 +27,10 @@ app.enable('trust proxy')
 // start template engine
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
+
+// cors
+app.use(cors())
+app.options('*', cors())
 
 // serve static file
 app.use(express.static(path.join(__dirname, 'public')))
