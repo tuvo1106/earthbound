b83c3ee7964bb59f2f3987d385ded03e2f338a67
Tu <tuvo1106@gmail.com>
2019-09-14 18:50:21 -0700

    Remove unnecessary functions

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { getCheckoutSession } = require('./../controllers/bookingController')
-const { protect, restrictTo } = require('./../controllers/authController')
+const { protect } = require('./../controllers/authController')
 
 const router = express.Router()
 
