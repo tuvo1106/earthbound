ac83db079e5443fd2fa868df00f63897cfd7887c
Tu Vo <tuvo1106@gmail.com>
2020-06-08 17:12:54 -0700

    Update viewsController

--- controllers/viewsContoller.js
+++ controllers/viewsContoller.js
@@ -8,39 +8,40 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find()
   res.status(200).render('overview', {
     title: 'Home',
-    tours
+    tours,
   })
 })
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: 'review rating user'
+    fields: 'review rating user',
   })
   if (!tour) {
     return next(new AppError('There is no tour with that name.'), 404)
   }
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
-    tour
+    tour,
   })
 })
 
 exports.login = catchAsync(async (req, res, next) => {
   res.status(200).render('login', {
-    title: 'Login'
+    title: 'Login',
   })
 })
 
 exports.signUp = catchAsync(async (req, res, next) => {
-  res.status(200).render('signUp', {
-    title: 'Sign Up'
+  console.log('end?')
+  res.status(200).render('signup', {
+    title: 'Sign Up',
   })
 })
 
 exports.getAccount = (req, res) => {
   res.status(200).render('account', {
-    title: 'Your account'
+    title: 'Your account',
   })
 }
 
@@ -49,16 +50,16 @@ exports.updateUserData = catchAsync(async (req, res, next) => {
     req.user.id,
     {
       name: req.body.name,
-      email: req.body.email
+      email: req.body.email,
     },
     {
       new: true,
-      runValidators: true
+      runValidators: true,
     }
   )
   res.status(200).render('account', {
     title: 'Your account',
-    user: updatedUser
+    user: updatedUser,
   })
 })
 
@@ -66,11 +67,11 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
   // find all bookings
   const bookings = await Booking.find({ user: req.user.id })
   // find tours with returned IDs
-  const tourIDs = bookings.map(el => el.tour)
+  const tourIDs = bookings.map((el) => el.tour)
   // check _id in give array
   const tours = await Tour.find({ _id: { $in: tourIDs } })
   res.status(200).render('overview', {
     title: 'My Tours',
-    tours
+    tours,
   })
 })
